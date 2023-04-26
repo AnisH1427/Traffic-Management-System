bff65a2f00bf05a4598552e07cf4ded71be71fa2
Anish Khatiwada <np03cs4s220208@heraldcollege.edu.np>
2023-04-26 19:35:22 +0545

    update vehicles file

--- offender/vehicles.php
+++ offender/vehicles.php
@@ -3,11 +3,11 @@
 
 <head>
     <?php include("../admin/assets/head.php"); ?>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css" />
 
-    <!-- <link rel="stylesheet" href="../admin/assets/css/Trafficpoli.css"> -->
-    <link rel="stylesheet" href="offender.css">
+    <link rel="stylesheet" href="../admin/assets/css/trafficpolis.css">
     <title>Vehicles</title>
 </head>
 
@@ -21,46 +21,115 @@
             <!-- Bootstrap CSS -->
             <!-- Button trigger modal -->
 
-            <!-- Add Student -->
+            <!-- Add Vehicles -->
             <div class="modal fade" id="policeAddModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Add Vehicles</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <form id="savePolice">
-                            <div class="modal-body">
-
-                                <div id="errorMessage" class="alert alert-warning d-none"></div>
-
-                                <div class="mb-3">
-                                    <label for="">Vehicle number</label>
-                                    <input type="text" name="name" class="form-control" />
-                                </div>
-                                <div class="mb-3">
-                                    <label for="">Owner Name</label>
-                                    <input type="text" name="phone" class="form-control" />
-                                </div>
-                                <div class="mb-3">
-                                    <label for="">Plate no</label>
-                                    <input type="text" name="email" class="form-control" />
-                                </div>
-                                <div class="mb-3">
-                                    <label for="">Vehicles Type</label>
-                                    <input type="text" name="location" class="form-control" />
-                                </div>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                <button type="submit" class="btn btn-primary">Save Police</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Add Vehicles</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
+            <form id="savePolice" method="post">
+                <div class="modal-body">
 
-            <!-- Edit Police Modal -->
+                    <div id="errorMessage" class="alert alert-warning d-none"></div>
+
+                    <div class="mb-3">
+                        <label for="">License Plate No</label>
+                        <input type="text" name="License_Plate_No" class="form-control" />
+                    </div>
+                    <div class="mb-3">
+                        <label for="">Owner Name</label>
+                        <select class="form-control" name="Owner_Name">
+                        <option value="">Select Vehicle's Owner</option>
+                            <?php
+                            // Connect to the database
+                            require_once 'connect.php';
+                            
+                            // Retrieve the list of user names
+                            $sql = "SELECT Name FROM user";
+                            $result = $conn->query($sql);
+                            while ($row = $result->fetch_assoc()) {
+                                echo '<option value="' . $row['Name'] . '">' . $row['Name'] . '</option>';
+                            }
+                            ?>
+                        </select>
+                    </div>
+                    <div class="dropdown mb-3">
+                        <label for="">Vehicle Type</label>
+                        <select class="form-control" name="Vehicle_Type">
+                            <option value="">Select Vehicle Type</option>
+                            <option value="Two-Wheelers">Two-Wheelers</option>
+                            <option value="Three-Wheelers">Three-Wheelers</option>
+                            <option value="Four-Wheelers">Four-Wheelers</option>
+                            <option value="Six-Wheelers">Six-Wheelers</option>
+                            <option value="Eight-Wheelers">Eight-Wheelers</option>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="">Manufacturer</label>
+                        <input type="text" name="Manufacturer" class="form-control" />
+                    </div>   
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary" name="saveVehicle">Save Vehicle</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+// establish database connection
+require_once('connect.php');
+
+// check if form is submitted
+if (isset($_POST['saveVehicle'])) {
+    // initialize error message
+    $errorMessage = 'Error, while adding vehicles';
+
+    // check if all required fields are filled
+    if (empty($_POST['License_Plate_No']) || empty($_POST['Owner_Name']) || empty($_POST['Vehicle_Type']) || empty($_POST['Manufacturer'])) {
+        $errorMessage = 'Please fill all required fields';
+    } else {
+        // sanitize input values to prevent SQL injection
+        $license_plate_no = mysqli_real_escape_string($conn, $_POST['License_Plate_No']);
+        $owner_name = mysqli_real_escape_string($conn, $_POST['Owner_Name']);
+        $vehicle_type = mysqli_real_escape_string($conn, $_POST['Vehicle_Type']);
+        $manufacturer = mysqli_real_escape_string($conn, $_POST['Manufacturer']);
+
+        // check if plate number already exists in database
+        $query = "SELECT * FROM vehicle_info WHERE License_Plate_No='$license_plate_no'";
+        $result = mysqli_query($conn, $query);
+        if (mysqli_num_rows($result) > 0) {
+            $errorMessage = 'Plate number already exists';
+        } else {
+            // get owner id from selected owner name
+            $query = "SELECT * FROM user WHERE Name='$owner_name'";
+            $result = mysqli_query($conn, $query);
+            $row = mysqli_fetch_assoc($result);
+            $owner_id = $row['Id'];
+
+            // insert vehicle information into database
+            $query = "INSERT INTO vehicle_info (License_Plate_No, Owner_Name, Vehicle_Type, Manufacturer) VALUES ('$license_plate_no', '$owner_name', '$vehicle_type', '$manufacturer')";
+            if (mysqli_query($conn, $query)) {
+                echo '<script>alert("Vehicle added successfully")</script>';
+            } else {
+                $errorMessage = 'Error: ' . mysqli_error($conn);
+            }
+        }
+    }
+
+    // if there is any error, show error message
+    if (!empty($errorMessage)) {
+        echo '<script>alert("Error: ' . $errorMessage . '")</script>';
+    }
+}
+?>
+            <!-- Edit Vehicle Modal -->
             <div class="modal fade" id="policeEditModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
@@ -74,23 +143,33 @@
                                 <div id="errorMessageUpdate" class="alert alert-warning d-none"></div>
 
                                 <input type="hidden" name="police_id" id="police_id">
-
                                 <div class="mb-3">
-                                    <label for="">Name</label>
-                                    <input type="text" name="name" id="Name" class="form-control" />
+                                    <label for="">License Plate</label>
+                                    <input type="text" name="name" class="form-control" />
+                                </div>
+                                <div class = "dropdown">
+                                <a class="btn btn-secondary dropdown-toggle dropDownTitle" role="button" data-bs-toggle="dropdown" aria-expanded="false" value="0">
+                                        Owner Name
+                                    </a>
+                                    <!-- <ul class="dropdown-menu putFromDatabase">
+                                    </ul> -->
                                 </div>
                                 <div class="mb-3">
-                                    <label for="">MobileNumber</label>
-                                    <input type="text" name="phone" id="Mobilenumber" class="form-control" />
-                                </div>
-                                <div class="mb-3">
-                                    <label for="">Email</label>
-                                    <input type="text" name="email" id="Email" class="form-control" />
-                                </div>
-                                <div class="mb-3">
-                                    <label for="">Location_Name</label>
-                                    <input type="text" name="location" id="LocationName" class="form-control" />
-                                </div>
+                                    <label for="">Plate no</label>
+                                    <input type="text" name="email" class="form-control" />
+                                </div> 
+                                <div class="dropdown">
+                                    <button class="btn btn-secondary dropdown-toggle dropDownTitle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Vehicle Type
+                                    </button>
+                                    <ul class="dropdown-menu putPoliceFromDatabase">
+                                    </ul>
+                                    </div>
+                                    <br>
+                                    <div class="mb-3">
+                                    <label for="">Manufacturer</label>
+                                    <input type="text" name="email" class="form-control" />
+                                </div>   
                             </div>
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -152,10 +231,10 @@
                                 <table id="myTable" class="table table-bordered table-striped">
                                     <thead>
                                         <tr>
+                                            <th>Id</th>
                                             <th>License<br>Plate No</th>
                                             <th>Owner <br>Name</th>
                                             <th>Vehicle<br>Type</th>
-                                            <th>Mobile<br>Number</th>
                                             <th>Manufacturer</th>
                                             <!-- <th>Location</th> -->
                                             <th>Action</th>
@@ -172,16 +251,16 @@
                                             foreach ($query_run as $vehicle) {
                                         ?>
                                                 <tr>
-                                                    <td><?= $vehicle['vec_num'] ?></td>
+                                                    <td><?= $vehicle['Id'] ?></td>
+                                                    <td><?= $vehicle['License_Plate_No'] ?></td>
                                                     <td><?= $vehicle['Owner_Name'] ?></td>
                                                     <td><?= $vehicle['Vehicle_Type'] ?></td>
-                                                    <td><?= $vehicle['License_Plate_No'] ?></td>
                                                     <td><?= $vehicle['Manufacturer'] ?></td>
 
                                                     <td>
-                                                        <button type="button" value="<?= $vehicle['vec_num'] ?>" class="viewPoliceBtn btn btn-info btn-sm">View</button>
-                                                        <button type="button" value="<?= $vehicle['vec_num'] ?>" class="editPoliceBtn btn btn-success btn-sm">Edit</button>
-                                                        <button type="button" value="<?= $vehicle['vec_num'] ?>" class="deletePoliceBtn btn btn-danger btn-sm">Delete</button>
+                                                        <button type="button" value="<?= $vehicle['Id'] ?>" class="viewPoliceBtn btn btn-info btn-sm">View</button>
+                                                        <button type="button" value="<?= $vehicle['Id'] ?>" class="editPoliceBtn btn btn-success btn-sm">Edit</button>
+                                                        <button type="button" value="<?= $vehicle['Id'] ?>" class="deletePoliceBtn btn btn-danger btn-sm">Delete</button>
                                                     </td>
                                                 </tr>
                                         <?php
@@ -203,13 +282,14 @@
     </html> -->
             <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
             <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+            
             <script>
                 // MenuToggle
                 let toggle = document.querySelector('.toggle');
                 let navigation = document.querySelector('.navigation');
                 let main = document.querySelector('.main');
 
-                toggle.onclick = function() {
+             6   toggle.onclick = function() {
                     navigation.classList.toggle('active');
                 }
                 // add hovered class in selected list item
