c32503154b3149b6ce846a9bf67b439f2bda5393
Anish Khatiwada <np03cs4s220208@heraldcollege.edu.np>
2023-05-02 10:49:38 +0545

    Added all files from the src folder

--- Asstes/dbConn.php
+++
@@ -1,3 +0,0 @@
-<?php
-// DB connection
-$conn = new mysqli("127.0.0.1:3307", "root", "", "tms") or die("Connection failed: " . $conn->connect_error);
--- User/preFunction/userInfo.php
+++
@@ -1,5 +0,0 @@
-<div class="w3-container">
-    <img src="<?= $userInfo['pic'] ?>" style=" width: 35%;" class="w3-round"><br><br>
-    <h4><b><?= $vecInfo["Owner_Name"]; ?></b></h4>
-    <p class="w3-text-grey">Owner of '<?= $vecInfo["vec_num"] ?>'</p>
-</div>
